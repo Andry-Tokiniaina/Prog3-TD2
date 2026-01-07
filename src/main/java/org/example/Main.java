68e940a2802133e8dd0d9e7ba23ef86c23dbeaf4
Toky <tokiniainarakotozanany@gmail.com>
2026-01-07 08:40:01 +0300

    un comment the tests who could throw exeption and add getGrossMargin test in saveDish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,22 +15,19 @@ public class Main {
         System.out.println(dataRetriever.findIngredientsByCriteria(null, CategoryEnum.VEGETABLE, null, 1, 10));
         System.out.println(dataRetriever.findIngredientsByCriteria("cho", null, "Sal", 1, 10));
         System.out.println(dataRetriever.findIngredientsByCriteria("cho", null, "gateau", 1, 10));
-        //System.out.println(dataRetriever.createIngredients(List.of(new Ingredient( "Fromage", 1200.00, CategoryEnum.DAIRY),
-        //        new Ingredient( "Oignon", 500.00, CategoryEnum.VEGETABLE, null))));
-        //System.out.println(dataRetriever.createIngredients(List.of(new Ingredient("Carotte", 2000.00, CategoryEnum.VEGETABLE),
-        //       new Ingredient("Laitue", 2000.00, CategoryEnum.VEGETABLE))));
-
-
+        System.out.println(dataRetriever.createIngredients(List.of(new Ingredient( "Fromage", 1200.00, CategoryEnum.DAIRY),
+                new Ingredient( "Oignon", 500.00, CategoryEnum.VEGETABLE, null))));
+        System.out.println(dataRetriever.createIngredients(List.of(new Ingredient("Carotte", 2000.00, CategoryEnum.VEGETABLE),
+               new Ingredient("Laitue", 2000.00, CategoryEnum.VEGETABLE))));
         System.out.println(dataRetriever.saveDish(new Dish("Soupe de légumes", DishTypeEnum.STARTER, List.of(dataRetriever.findIngredientByName("Oignon")))));
-
         System.out.println(dataRetriever.saveDish(new Dish(1, "Salade fraîche", DishTypeEnum.STARTER,
                 List.of(dataRetriever.findIngredientByName("Oignon"), dataRetriever.findIngredientByName("Laitue"),
                         dataRetriever.findIngredientByName("Tomate"),  dataRetriever.findIngredientByName("Fromage")))));
+
+
         System.out.println(dataRetriever.saveDish(new Dish(1, "Salade de fromage", DishTypeEnum.STARTER, 2000.00,
-                 List.of(dataRetriever.findIngredientByName("Fromage")))));
-
-
-        //System.out.println(dataRetriever.findDishById(1).getGrossMargin());
+                 List.of(dataRetriever.findIngredientByName("Fromage")))).getGrossMargin());
+        System.out.println(dataRetriever.findDishById(1).getGrossMargin());
         System.out.println(dataRetriever.findDishById(2).getGrossMargin());
     }
 }
